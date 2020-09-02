4ca210572ca467da360016a15899a5c7f82bac8e
ArashZich <zenith.arash@gmail.com>
2020-09-02 14:26:51 +0430

    handle server but not response

--- src/Component/Screen/Home.js
+++ src/Component/Screen/Home.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Button } from "react-native-web";
-import FontAwesomeIcon from "react-native-vector-icons/dist/FontAwesome";
-import EntypoIcon from "react-native-vector-icons/dist/Entypo";
+import axios from "axios";
 
 const Home = ({ navigation }) => {
+  const [state, setState] = useState({});
+  const [list, setList] = useState([]);
+
+  axios.interceptors.request.use(function (config) {
+    const token =
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InRoY29iMDg5NyIsIm5hbWVpZCI6Ijk0MCIsImdyb3Vwc2lkIjoiNTQwIiwiY2VydHNlcmlhbG51bWJlciI6ImFwdmFvLWJ3Nmk2LTluNHRqLWVkbnU4IiwibmJmIjoxNTk5MDMzODEyLCJleHAiOjE1OTkyOTMwMTIsImlhdCI6MTU5OTAzMzgxMn0.nPdeAvkLgZSBtULBgWDD0BzWBortEYVMpaPSzBVC9Y4";
+
+    config.headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Headers":
+        "Origin,Cache-Control,Accept,X-Access-Token ,X-Requested-With, Content-Type, Access-Control-Request-Method",
+      Authorization: token,
+    };
+
+    return config;
+  });
+  const getToken = () => {
+    axios({
+      method: "post",
+      url: "http://crmapi.cobeldarou.com/api/Token",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      data: {
+        UserName: "thcob0897",
+        Password: "20151",
+      },
+    })
+      .then((res) => setState(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  const getPerson = () => {
+    axios({
+      method: "get",
+      url: "http://crmapi.cobeldarou.com/api/Person",
+    })
+      .then((res) => setList(res.data))
+      .catch((err) => console.log(err));
+  };
+
   const handlePageFour = () => {
     navigation.navigate("PageFour");
   };
@@ -16,8 +60,17 @@ const Home = ({ navigation }) => {
       <Text style={{ textAlign: "center" }}>This is Home Screen!</Text>
       <Button onPress={handlePageFour} title="Page Four" />
       <Button onPress={handleSharePage} title="Share Page" />
-      <FontAwesomeIcon name="pied-piper-alt" size={30} color="#010" />
-      <EntypoIcon name="bug" size={30} />
+      <Button onPress={getToken} title="Token" />
+      <Button onPress={getPerson} title="Person" />
+      <Text style={{ textAlign: "center", fontSize: 10 }}>{state.Token}</Text>
+      {list &&
+        list.map((item, ind) => {
+          return (
+            <Text key={ind} style={{ textAlign: "center", fontSize: 10 }}>
+              {item.label}
+            </Text>
+          );
+        })}
     </View>
   );
 };
